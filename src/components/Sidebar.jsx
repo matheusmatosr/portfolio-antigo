96c63bc2f95821aa955bb9b31f6df4bcb2cc2497
Matheus Matos <100291312+matheusmatosr@users.noreply.github.com>
2023-01-21 10:16:10 -0300

    test
    test

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ import "../styles/components/sidebar.sass";
 const Sidebar = () => {
   return (
     <aside id="sidebar">
-      <img src={Avatar} alt="Matheus Matos" />
+      <img src={Avatar} alt="Matheus Matos" className="imgEu"/>
       <p className="title">Desenvolvedor web</p>
       <SocialNetworkContainer />
       <InformationContainer />
